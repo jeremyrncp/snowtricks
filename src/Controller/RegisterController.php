77880c7af54c9302b76ebf55a62626f3a2b58eb6
Jérémy Gaultier RNCP <webmezenc@gmail.com>
2018-10-11 18:14:51 +0200

    Feature finalisation: Index template created Register template created

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function index()
+    {
+        return $this->render('register.html.twig' );
+    }
+}
