36442920c0a3feea2e0bfe54d8bf435cbc577ac5
Jérémy Gaultier RNCP <webmezenc@gmail.com>
2018-11-03 17:19:04 +0100

    feat(view-tricks-list): View tricks list
    Feature finalisation

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,19 +1,30 @@
+import {Spinner} from "./module/spinner";
+
 const $ = require('jquery');
 
 const selectorBtnMoreTricks = '#moreTricks';
 const selectorContainerListTricks = '#listTricks';
+const selectorSpinner = '#spinner';
 
 module.exports = (function() {
     const btnMoreTricks = document.querySelector(selectorBtnMoreTricks);
+    const spinner = new Spinner();
+          spinner.setSelector(selectorSpinner);
 
     btnMoreTricks.addEventListener('click', function (event) {
         const length = 10 + Number(btnMoreTricks.getAttribute('data-length'));
                         btnMoreTricks.setAttribute('data-length', String(length));
+
+        spinner.showSpinner();
+
         $.ajax({
             'data': {
                 'end':  length
             },
             'url': '/api/ressources/tricks',
+            'complete': function() {
+                spinner.hideSpinner();
+            },
             'success': function(res) {
                 const tricksContainer = document.querySelector(selectorContainerListTricks);
                       tricksContainer.innerHTML = res;
