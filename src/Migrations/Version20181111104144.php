7308748e24ae451583908aeca8a70a7af42696ed
Jérémy Gaultier RNCP <webmezenc@gmail.com>
2018-11-17 18:42:24 +0100

    tech(migrations): migrations for this feature

---
+++ src/Migrations/Version20181111104144.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181111104144 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE movies ADD url_movie VARCHAR(255) NOT NULL, DROP embed_code');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE movies ADD embed_code LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, DROP url_movie');
+    }
+}
