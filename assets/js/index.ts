964ad34f156fea973d9f770f6c8b397ee686f0d4
Jérémy Gaultier RNCP <webmezenc@gmail.com>
2018-11-24 13:43:12 +0100

    tech(front): add modal confirmation
    add modal confirmation with redirection in index

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,10 +1,13 @@
 import {Spinner} from "./module/spinner";
+import {ConfirmationWithRedirection} from "./module/ConfirmationWithRedirection";
+import {SwalConfirmAndRedirectionModal} from "./module/Infrastructure/Modal/SwalConfirmAndRedirectionModal";
 
 const $ = require('jquery');
 
 const selectorBtnMoreTricks = '#moreTricks';
 const selectorContainerListTricks = '#listTricks';
 const selectorSpinner = '#spinner';
+const selectorDeleteWithConfirmationModal = '.delete-with-confirmation';
 
 module.exports = (function() {
     const btnMoreTricks = document.querySelector(selectorBtnMoreTricks);
@@ -32,6 +35,28 @@ module.exports = (function() {
             'error': function(xhr, status, error) {
                 alert('An error has occurred during tricks recuperation ' + status);
             }
+        });
+    });
+
+
+    const deleteTricks = document.querySelectorAll(selectorDeleteWithConfirmationModal);
+    const confirmAndRedirectionModal = new SwalConfirmAndRedirectionModal();
+
+    Array.from(deleteTricks).forEach(trick => {
+        trick.addEventListener('click', function(event) {
+            event.stopImmediatePropagation();
+            event.stopPropagation();
+
+            let path = trick.getAttribute('data-path');
+
+            let confirmationWithRedirection = new ConfirmationWithRedirection(confirmAndRedirectionModal);
+            confirmationWithRedirection.showConfirmAndRedirectModal(
+                'Delete trick',
+                'Do you really want delete this trick and related medias ?',
+                'No',
+                'Yes',
+                path
+            );
         });
     });
 })();
